02c3a8ce66c9b90c8ad7d87af7fc060946fc5500
JcMoz <31970838+JcMoz@users.noreply.github.com>
2017-10-24 09:19:37 -0600

    guardaFoto
    guardaFoto

--- docente/ingresarDocente.php
+++ docente/ingresarDocente.php
@@ -1,6 +1,8 @@
 <?php
-include_once '../plantilla/incio_plantilla.php';;
-include_once '../plantilla/menu_navegacion.php';;
+include_once '../plantilla/incio_plantilla.php';
+;
+include_once '../plantilla/menu_navegacion.php';
+;
 ?>
 <style >
     .btn-cancelar{
@@ -14,21 +16,23 @@ include_once '../plantilla/menu_navegacion.php';;
     <div class="container-fluid"> <!--Comienza container Fluid-->
 
         &nbsp &nbsp &nbsp<font face="Arial Narrow" size="5" color="#001f4d">Ingresar docente.</font>
-        <div class="row">
-            <div class="col-md-8">
-                <div class="panel panel-default">
-                    <br>
-                    <div class="panel-heading" align="center">Datos de docente</div>
-                    <div class="panel-body">
+        <form action="" id="FORMULARIO_VALIDADO"  method="post" class="form-register" enctype="multipart/form-data" >
+            <div class="row"> 
+
+                <div class="col-md-8">
+                    <div class="panel panel-default">
                         <br>
-                        <!--comienza formulario-->
-                        <form id="FORMULARIO_VALIDADO"  method="post" class="form-register" >
+                        <div class="panel-heading" align="center">Datos de docente</div>
+                        <div class="panel-body">
+                            <br>
+                            <!--comienza formulario-->
+
                             <input type="hidden" name="tirar" id="pase"/>
 
                             <div class="row">
                                 <div class="col-md-1"></div>
                                 <div class="col-md-5">
-                                  
+
                                     <INPUT class="form-control" id="Idnom" name="nombreDo" type="text"  placeholder=" Nombres del docente" required="" minlength="2" pattern="[a-z]{1,15}">
                                 </div>
                                 <div class="col-md-5">
@@ -97,26 +101,35 @@ include_once '../plantilla/menu_navegacion.php';;
 
                             </div>
                             <br>
-                        </form><!--termina formulario-->
-                    </div><!--cierre del panel body-->
-                    <br>
 
-                    <br>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel">
-                        <!--imagen   -->
-                        <div align="center">
-                            <img src="../imagenes/docente1.png" class="img-responsive">
-                        </div>
+                        </div><!--cierre del panel body-->
+                        <br>
+
+                        <br>
+                        <br>
                     </div>
-                    <br> <br> <br>
-
                 </div>
-            </div>
-        </div>
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel">
+                            <!--imagen   -->
+                            <div align="center">
+                                <br/>
+                                <img src="../imagenes/docente1.png" class="img-responsive">
+                            </div>
+                            <br/>
+                            <div id="fotos1">
+                                <p id="te">Agregar foto</p>
+                                <input class="form-control "type="file" name="imagenG" id="foto"/><br/><br/>
+                            </div>
+                        </div>
+                        <br> <br> <br>
+
+                    </div>
+                </div>
+
+            </div><!--row-->
+        </form><!--termina formulario-->
         </form><!--fin form-->
 
     </div><!--Termina container fluid-->
@@ -137,15 +150,15 @@ if (isset($_REQUEST['tirar'])) {
     $nt = $_POST["nit"];
     $du = $_POST["dui"];
     $esp = $_POST["esp"];
+    $img= addslashes(file_get_contents($_FILES['imagenG']['tmp_name']));
 
-    mysqli_query($conexion,"INSERT INTO docente(nom_doc,ape_doc,dir_doc,tel_doc,gen_doc,f_nac_doc,cor_doc,nip_doc,nit,dui_doc,esp_doc) VALUES ('$nombre','$apellido','$direc','$tel','$sexo','$fec','$co','$np','$nt','$du','$esp')");
-   
+    mysqli_query($conexion, "INSERT INTO docente(nom_doc,ape_doc,dir_doc,tel_doc,gen_doc,f_nac_doc,cor_doc,nip_doc,nit,dui_doc,esp_doc,foto_doc) VALUES ('$nombre','$apellido','$direc','$tel','$sexo','$fec','$co','$np','$nt','$du','$esp','$img')");
 }
 include_once '../plantilla/fin_plantilla.php';
 ?>
 <script type="text/javascript">
- $('.mask-dui').mask('00000000-0');
- $('.mask-telefono').mask('0000-0000');
-  $('.mask-nit').mask('0000-000000-000-0');
- </script>
+    $('.mask-dui').mask('00000000-0');
+    $('.mask-telefono').mask('0000-0000');
+    $('.mask-nit').mask('0000-000000-000-0');
+</script>
 
