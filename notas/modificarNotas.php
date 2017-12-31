36755501ff64645cb8cabbb55f56244c098ce5e2
JcMoz <31970838+JcMoz@users.noreply.github.com>
2017-12-30 21:52:34 -0600

    ModificacionNotas
    termine la modificacion de las notas

--- notas/modificarNotas.php
+++ notas/modificarNotas.php
@@ -1,6 +1,31 @@
 <?php
+session_start();
+include_once '../conexion/php_conexion.php';
 include_once '../plantilla/incio_plantilla.php';
-include_once '../plantilla/menu_navegacion.php';
+try {
+    if ($_SESSION['tipo_user'] == 'ad' or $_SESSION['tipo_user'] == 'p') {
+        $profesor = $_SESSION['id_profesor'];
+        $sacar = mysqli_query($conexion, "SELECT*FROM docente WHERE id_doc='$profesor'");
+
+        while ($row = mysqli_fetch_array($sacar)) {
+            $nombre = $row['nom_doc'];
+            $ape = $row['ape_doc'];
+        }
+    }
+} catch (Exception $exc) {
+    echo '<script>swal("EROR", "Favor revisar los datos e intentar nuevamente", "error");</script>';
+}
+?>
+<?php
+//body
+include_once '../plantilla/incio_plantilla.php';
+if ($_SESSION['tipo_user'] == 'ad') {
+    include_once '../plantilla/menu_navegacion.php';
+} else {
+    if ($_SESSION['tipo_user'] == 'p') {
+        include_once '../plantilla/menu_navegacion_1.php';
+    }
+}
 ?>
 <!--comienza mi codigo-->
 <div class="content-wrapper">
@@ -8,92 +33,114 @@ include_once '../plantilla/menu_navegacion.php';
     <div class="container-fluid">
     	<!--Encabezado de la pantalla-->
     <div class="row-fluid" align="center">
+        <br/> <br/>
                             <div class="span6">
                               <h2 class="text-info">
-                                    <img src="imagenes/bus.png" width="50" height="50">
-                                    Buscar Notas
+                                  
+                                  Seleccione: grado, materia, periodo<img src="../imagenes/fecha.png" width="50" height="50">
                                 </h2>
                             </div>
                             <div class="span6">
-                              <form name="form1" method="post" action="">
-                                  <div class="input-append">
-                                  <input type="text" name="buscar" class="input-xlarge" autocomplete="off" autofocus placeholder="         Alumno ">
-                                    <button type="submit" class="btn-secondary">
-                                    <strong>
-                                    <i class="icon-search"></i> Buscar
-                                    </strong>
-                                    </button>
-                                     
-                                    </div>
+                                <form action="" id="FORMULARIO_VALIDADO"  method="post" class="form-register" >
+                                    
+                                <div class="row">
+                                    <div class="col-md-2"></div>
+                                <div class="col-md-3">    
+                  <!--****para los grados en el combo-->  
+                  <select class="form-control" name="grados" >
 
-                                </form>
-                              <br>
+                    <?php
+                    include_once '../conexion/php_conexion.php';
+                   $pagrado = mysqli_query($conexion, "SELECT * FROM grado");
+                                    while ($row = mysqli_fetch_array($pagrado)) {
+                                        ?>
+                               <option value="<?php echo $row['id_grado'];?>"><?php echo $row['nom_grado']; ?></option>
+                                    
+                                <?php    }
+                                    ?>
+                      
+                                </select>
+                  <!--****para los grados en el combo--> 
+                                </div><!--columna de 3-->
+                                <div class="col-md-3"> 
+                         <!--****para las materias en el combo-->  
+                  <select class="form-control" name="materias" >
+
+                    <?php
+                    include_once '../conexion/php_conexion.php';
+                   $pamateria = mysqli_query($conexion, "SELECT * FROM materias");
+                                    while ($row = mysqli_fetch_array($pamateria)) {
+                                        ?>
+                               <option value="<?php echo $row['id_materia'];?>"><?php echo $row['nombre']; ?></option>
+                                    
+                                <?php    }
+                                    ?>
+                      
+                                </select>
+                  <!--****para las materias en el combo--> 
+                                </div><!--columna de 3-->
+                   <!--columnas de 3-->             
+                  <div class="col-md-3">               
+                 <!--para los periodos-->               
+                 <select class="form-control" name="periodo">
+                    <option value=1>Primer</option>
+                    <option value=2>Segundo</option>
+                    <option value=3">Tercer</option>
+                </select>
+                 <!--fin periodos-->
+                  </div><!--fin de columnas de 3-->
+                              <div class="col-md-1"></div>
+                                </div<!--rooooow-->
                             </div>
+        <br/>
+        <input type="submit" value="ver" name="procesar" class=" btn btn-outline-primary" />
+                   </form>
                         </div> <!--Fin de Encabezado de la pantalla-->
-           <div>
-            &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp<input class="form-group" type="text" name="maestro" size="50" placeholder="        Nombre del Maestro">  &nbsp &nbsp
-            <input class="form-group" type="text" name="grado" size="15" placeholder="          Grado">  &nbsp &nbsp
-            <input class="form-group" type="text" name="ano" size="15" placeholder="          Año">
-            <br>
-            &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp<input class="form-group" type="text" name="asig" size="30" placeholder="        Asignatura">  &nbsp &nbsp &nbsp
-            <input class="form-group" type="text" name="period" size="20" placeholder="             Periodo">
-        </div>
+                         <br/>
         <!--**************************codigo de la tabla ***********************************-->
         <div class="row">
             <div class="col">
                 <!--Comienza tabla-->
         <div class="panel-body">
-            <table class="table table-bordered table table-hover table-sm table-responsive">
-                <thead class="">
-                <th class="text-center">NIE</th>
-                <th class="text-center">Alumnos</th>
-                <th class="text-center">Act1</th>
-                <th class="text-center">Act2</th>
-                <th class="text-center">Act3</th>
-                <th class="text-center">R</th>
-                <th class="text-center">P</th>
-                <th class="text-center">Act1</th>
-                <th class="text-center">Act2</th>
-                <th class="text-center">Act3</th>
-                <th class="text-center">R</th>
-                <th class="text-center">P</th>
-                <th class="text-center">Act1</th>
-                <th class="text-center">Act2</th>
-                <th class="text-center">Act3</th>
-                <th class="text-center">R</th>
-                <th class="text-center">P</th>
-                <th class="text-center">PF</th>
-                <th class="text-center">Acción</th>
-                </thead>
+            <?php if (isset($_POST['grados'])&& isset($_POST['materias'])&& isset($_POST['periodo'])) {
+               
+                                  
+                              ?>
+            <table class="table table-bordered table table-active">
+                        <thead class="">
 
-                <tbody>
-                    <tr>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                                                
-                        <td class="text-center">
-                            <a class="btn btn-success btn-block btn-large" href="notasModificar.php">Modificar
-                            </a></td>
-                    </tr>
+                        <th class="text-center">NIE</th>
+                        <th class="text-center">Nombre</th>
+                        <th class="text-center">Apellido</th>
+                        <th class="text-center">Acción</th>
 
-                </tbody>
-            </table>
+
+                        </thead>
+
+                        <tbody>
+                            <?php
+                             $graM=$_POST['grados'];
+                             $materiasM=$_POST['materias'];
+                             $peM=$_POST['periodo'];
+                            $alumnos = mysqli_query($conexion, "SELECT*FROM inscripcion i, alumno a, notas_2 n WHERE i.id_alumno=a.id_alumno AND i.id_inscripcion=n.id_inscripcion AND n.id_grado='$graM' AND n.id_materia='$materiasM' AND n.estado_n='procesado' AND periodo='$peM'");
+                            while ($row=  mysqli_fetch_array($alumnos)){
+                                $nombre=$row['nie'];
+                                $id=$row['id_inscripcion'];
+                                
+                            ?>
+
+                            <tr>
+                                <td class="text-center"><?php echo $row['nie'];?></td>
+                                <td class="text-center"><?php echo $row['nom_alumno'];?></td>
+                                <td class="text-center"><?php echo $row['ape_alumno'];?></td>
+                                <td class="text-center"><a href="../notas/registrar_notas_modificadas.php?ir=<?php echo $graM;?>&llego=<?php echo $materiasM;?>&id=<?php echo $id; ?>&pe=<?php echo $peM ;?>" class="btn">Modificar</a></td>
+
+                            </tr>
+                            <?php }?>
+                        </tbody>
+                    </table> <!--termina tabla-->
+                    <?php } ?>
+            
         </div>
         <!--termina tabla-->
             </div>
---
+++ notas/registrar_notas_modificadas.php
@@ -0,0 +1,224 @@
+<?php
+session_start();
+include_once '../conexion/php_conexion.php';
+include_once '../plantilla/incio_plantilla.php';
+try {
+    if ($_SESSION['tipo_user'] == 'ad' or $_SESSION['tipo_user'] == 'p') {
+        $profesor = $_SESSION['id_profesor'];
+        $sacar = mysqli_query($conexion, "SELECT*FROM docente WHERE id_doc='$profesor'");
+
+        while ($row = mysqli_fetch_array($sacar)) {
+            $nombre = $row['nom_doc'];
+            $ape = $row['ape_doc'];
+        }
+    }
+} catch (Exception $exc) {
+    echo '<script>swal("EROR", "Favor revisar los datos e intentar nuevamente", "error");</script>';
+}
+?>
+<?php
+//body
+include_once '../plantilla/incio_plantilla.php';
+if ($_SESSION['tipo_user'] == 'ad') {
+    include_once '../plantilla/menu_navegacion.php';
+} else {
+    if ($_SESSION['tipo_user'] == 'p') {
+        include_once '../plantilla/menu_navegacion_1.php';
+    }
+}
+$grado = $_GET['ir'];
+$materia = $_GET['llego'];
+$alumno = $_GET['id'];
+$periodo=$_GET['pe'];
+
+//********************Extraer id de asiganacion de grado c...
+$extraer_id = mysqli_query($conexion, "SELECT*FROM actividades LEFT JOIN asignacion_a_g ON actividades.id_asignacion_a_g=asignacion_a_g.id_asignacion WHERE id_gra='$grado' AND id_materia='$materia' AND estado_a='procesado' AND periodo='$periodo'");
+while ($xyz = mysqli_fetch_array($extraer_id)) {
+    $a_actividad = $xyz['id_asignacion'];
+    $pe=$xyz['periodo'];
+    $a1=$xyz['act_1'];
+    $a2=$xyz['act_2'];
+    $a3=$xyz['act_3'];
+    $a4=$xyz['act_4'];
+    $a5=$xyz['act_5'];
+    $a6=$xyz['act_6'];
+    $a7=$xyz['act_7'];
+    $a8=$xyz['act_8'];
+    $a9=$xyz['act_9'];
+}
+//*******************para sacar las notas
+$notas=  mysqli_query($conexion,"SELECT*FROM notas_2 WHERE id_inscripcion='$alumno' AND id_materia='$materia' AND id_grado='$grado' AND periodo='$pe' AND estado_n='procesado'");
+while ($ver_notas=  mysqli_fetch_array($notas)){
+    $n1=$ver_notas['nota1'];
+    $n2=$ver_notas['nota2'];
+    $n3=$ver_notas['nota3'];
+    $n4=$ver_notas['nota4'];
+    $id_update=$ver_notas['id'];
+}
+?>
+<script>
+        function validaNotas(e){
+        key=e.keyCode || e.which;
+        teclado=String.fromCharCode(key);
+        numerito="0123456789.";
+        especiales="37";
+        teclado_especial=false;
+        for(var i in especiales){
+            if(key==especiales[i]){
+                teclado_especial=true;
+            }
+        }
+        if(numerito.indexOf(teclado)==-1 && !teclado_especial){
+        return false;
+    }
+        }
+
+</script>
+<style >
+    .btn-atras{
+        background-color: #607d8b;
+        color: white;
+    }
+
+</style>
+<!--comienza mi codigo-->
+<div class="content-wrapper">
+    <!--Comienza container fluid-->
+    <form action="" id="FORMULARIO_VALIDADO"  method="post" class="form-register" >
+        <div class="container-fluid">
+            <?php
+//para extraer el nombre y apellido del alumno
+            $sacar = mysqli_query($conexion, "SELECT*FROM alumno INNER JOIN inscripcion on alumno.id_alumno=inscripcion.id_alumno WHERE id_inscripcion='$alumno'");
+            while ($alumnito = mysqli_fetch_array($sacar)) {
+                $nom_alumno = $alumnito['nom_alumno'];
+                $ape = $alumnito['ape_alumno'];
+            }
+//fin de codigo*************************************
+//*************fin de extraer datos
+            ?>
+            <div align="center">
+                <font face="Arial Narrow" size="5" color="#001f4d">Modificar notas</font>
+                <img src="../imagenes/modificarCali.png" width="80" height="80"><br/>
+                <font face="Arial Narrow" size="5" color="#001f">Alumno/a: <?php echo $nom_alumno; ?> <?php echo $ape; ?></font>
+            </div>
+            <input type="hidden" name="tirar" id="pase"/>
+
+
+            <div class="row"><!--comienza row-->
+
+
+                <div class="col-md-4"><!--comienza las columnas de la pantalla-->
+                    <div class="panel panel-default">
+                        <br/>
+                        <div class="panel-heading" align="center">Actividades 35%</div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-md-8">  
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a1;?>">
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a2;?>">
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a3;?>">
+                                    <br> 
+                                </div>
+                                <div class="col-md-4">  
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text" placeholder="0.0" value="<?php echo $n1;?>" name="nota1" onkeypress="return validaNotas(event)" onpaste="return false" maxlength="4">
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text" name="nota2" placeholder="0.0" value="<?php echo $n2;?>">
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text"  name="nota3" placeholder="0.0" value="<?php echo $n3;?>">
+                                   <br> 
+                                </div>
+                                <div class="col-md-1"></div>
+                            </div>
+                        </div><!--cierre del panel body-->
+                        <br/>
+                    </div>
+
+                </div><!--Termina las columnas de la pantalla-->
+
+                <div class="col-md-4"><!--comienza las columnas de la pantalla-->
+                    <div class="panel panel-default">
+                        <br/>
+                        <div class="panel-heading" align="center">Actividades 35%</div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-md-8">  
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a4;?>">
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a5;?>">
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a6;?>">
+                                    <br> 
+                                </div>
+                                <div class="col-md-3">  
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text"  name="nota4" placeholder="0.0">
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text"  name="nota5" placeholder="0.0">
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text"  name="nota6" placeholder="0.0">
+                                    <br> 
+                                </div>
+                                <div class="col-md-1"></div>
+                            </div>
+
+                        </div><!--cierre del panel body-->
+                        <br/>
+                    </div>
+                </div><!--Termina las columnas de la pantalla-->
+
+                <div class="col-md-4"><!--comienza las columnas de la pantalla-->
+                    <div class="panel panel-default">
+                        <br>
+                        <div class="panel-heading" align="center">Actividades 30%</div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-md-8">  
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a7;?>">
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a8;?>">
+                                    &nbsp<INPUT class="form-control" type="text" disabled="false" value="<?php echo $a9;?>">
+                                    <br> 
+                                </div>
+                                <div class="col-md-3">  
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text" name="nota7" placeholder="0.0">
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text"  name="nota8" placeholder="0.0">
+                                    &nbsp<INPUT class="form-control" autocomplete="off" autofocus type="text"  name="nota9" placeholder="0.0">
+                                    <br> 
+                                </div>
+                                <div class="col-md-1"></div>
+                            </div>
+
+                        </div><!--cierre del panel body-->
+                        <br>
+                    </div>
+                </div><!--Termina las columnas de la pantalla-->
+
+            </div><!--fin class row-->
+            <div align="center">
+                <input type="submit" value="Guardar" name="guardar" class="btn btn-info">
+                <input type="button" value="Cancelar" name="cancel" class=" btn btn-dark" onclick="location = '/proyectoDi/doc/principal.php'">
+            </div>
+
+        </div><!--terminada container fluid-->
+    </form><!--termina formulario-->
+</div><!--termina mi codigo-->
+<?php
+include_once '../plantilla/fin_plantilla.php';
+if (isset($_REQUEST['tirar'])) {
+    try {
+        
+    include_once '../conexion/php_conexion.php';
+    $not1 = $_REQUEST['nota1'];
+    $not2 = $_REQUEST['nota2'];
+    $not3 = $_REQUEST['nota3'];
+    $por35=  round((($not1+$not2+$not3)/3)*0.35,2);
+    mysqli_query($conexion, "UPDATE notas_2 SET nota1='$not1',nota2='$not2',nota3='$not3', por35='$por35' WHERE id='$id_update'");
+    echo '<script>swal({
+                    title: "Exito",
+                    text: "Calificaciones almacenadas",
+                    type: "success",
+                    confirmButtonText: "ok",
+                    closeOnConfirm: false
+                },
+                function () {
+                    location.href="ModificarNotas.php";
+                    
+                });</script>';
+    } catch (Exception $exc) {
+         echo '<script>swal("No se puedo realizar el registro", "Favor revisar los datos e intentar nuevamente", "error");</script>';
+               
+    }
+}
+?>
